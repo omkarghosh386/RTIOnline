56298a192396888265e8d7e69379ceecf6270606
tusuman <roysuman.mca@gmail.com>
2025-06-21 13:55:19 +0530

    delete rti-master
    delete rti-master

--- rti-master/src/main/java/nic/rti/master/dao/GetStateListRepository.java
+++
@@ -1,9 +0,0 @@
-package nic.rti.master.dao;
-
-import nic.rti.master.entity.GetStateList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GetStateListRepository extends JpaRepository<GetStateList, String> {
-}
